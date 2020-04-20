6aa35a5e8ea4445ea9a7d0f4f960da1dd4340f47
GracefulMan <41041321+GracefulMan@users.noreply.github.com>
2020-04-20 13:13:01 +0800

    线性表

---
+++ 线性表/链表存储.cpp
@@ -0,0 +1,119 @@
+//
+// Created by 马鸿英 on 2020-04-20.
+//
+#include <iostream>
+using namespace std;
+
+template <class T>
+class linkList{
+private:
+    struct node{
+        T data;
+        node *next;
+        node(const T &data, node *n = NULL):data(data), next(n){}
+        node():next(NULL){}
+    };
+    node *head;
+public:
+    linkList(){
+        head = new node();
+    }
+    int length()const{
+        int total_length = 0;
+        node *p = head->next;
+        while (p){
+            total_length += 1;
+            p = p->next;
+        }
+        return total_length;
+    }
+    int search(const T &x) const {
+        int index = -1;
+        node *p = head->next;
+        while (p){
+            index++;
+            if(p->data == x) return index;
+            p = p->next;
+        }
+        return -1;
+    }
+    T visit(int i) const {
+        node *p = head->next;
+        int index = 0;
+        while(p){
+            if(index == i) return p->data;
+            p = p->next;
+            index++;
+        }
+    }
+    void insert(int i, const T &x){
+        if(i<0) return;
+        int index = 0;
+        node *p = head;
+        while (index < i && p){
+            p = p->next;
+            index++;
+        }
+        if(!p) return;
+        node *tmp = new node(x,p->next);
+        p->next = tmp;
+    }
+    void remove(int i){
+        if(i < 0 )return;
+        int index = 0;
+        node *p = head;
+        while (index < i && p){
+            p = p->next;
+            index++;
+        }
+        if(!p) return;
+        node *tmp = p->next;
+        p->next = tmp->next;
+        delete tmp;
+    }
+    void clear(){
+        node *p = head->next;
+        head->next = NULL;
+        while(p){
+            node *tmp = p;
+            p = p->next;
+            delete tmp;
+        }
+    }
+    void traverse()const{
+        node *p = head->next;
+        while (p){
+            cout<<p->data<<'\t';
+            p = p->next;
+        }
+    }
+    ~linkList() {
+        node *p = head->next;
+        while (p) {
+            node *tmp = p;
+            p = p->next;
+            delete tmp;
+        }
+        delete head;
+    }
+
+};
+
+int main(){
+    linkList<int> test;
+    for(int i=0;i<100;i++){
+        test.insert(i,i);
+    }
+    test.traverse();
+    cout<<endl;
+    cout<<"current size:"<<test.length()<<endl;
+    cout<<"55:"<<test.visit(55)<<endl;
+    cout<<"search for 55:"<<test.search(55)<<endl;
+    for(int i=0;i<10;i++)
+        test.remove(0);
+    test.traverse();
+    cout<<endl;
+    cout<<"current size:"<<test.length()<<endl;
+    test.clear();
+    cout<<"current size:"<<test.length()<<endl;
+}
---
+++ 线性表/顺序存储.cpp
@@ -0,0 +1,94 @@
+//
+// Created by 马鸿英 on 2020-04-20.
+//
+#include <iostream>
+using namespace std;
+
+
+template <class T>
+class seqList{
+private:
+    int currentLength;
+    int maxSize;
+    T *data;
+    void doubleSpace(){
+        T *tmp = new T [maxSize * 2];
+        for(int i=0;i<currentLength;i++){
+            tmp[i] = data[i];
+        }
+        maxSize *= 2;
+        delete [] data;
+        data = tmp;
+    };
+public:
+    seqList(int maxsize=10):currentLength(0),maxSize(maxsize){
+        data = new T [maxsize];
+    }
+
+    int length()const {
+        return currentLength;
+    }
+
+    int search(const T &x) const {
+        for(int i=0;i<currentLength;i++){
+            if(data[i] == x) return i;
+        }
+        return -1;
+    }
+
+    T visit(int i) const {
+        if (i>=currentLength || i<0){
+            cerr<<"error index"<<endl;
+            exit(0);
+        }
+        return data[i];
+    }
+
+    void insert(int i,const T &x){
+        if(i < 0 || i > currentLength) return;
+        if(currentLength == maxSize){
+            doubleSpace();
+        }
+        for(int j=currentLength;j>i;j--)
+            data[j] = data[j-1];
+        data[i] = x;
+        currentLength++;
+    }
+
+    void remove(int i){
+        if(i < 0 || i >=currentLength) return;
+        for(int j=i;j<currentLength - 1; j++)
+            data[j] = data[j + 1];
+        currentLength--;
+    }
+    void clear(){
+        currentLength = 0;
+    }
+    void traverse()const {
+        for(int i=0;i<currentLength;i++)
+            cout<<data[i]<<'\t';
+    }
+    ~seqList(){
+        delete [] data;
+    }
+};
+
+
+int main(){
+    seqList<int> test;
+    for(int i=0;i<100;i++){
+        test.insert(i,i);
+    }
+    test.traverse();
+    cout<<endl;
+    cout<<"current size:"<<test.length()<<endl;
+    cout<<"55:"<<test.visit(55)<<endl;
+    cout<<"search for 55:"<<test.search(55)<<endl;
+    for(int i=0;i<10;i++)
+        test.remove(0);
+    test.traverse();
+    cout<<endl;
+    cout<<"current size:"<<test.length()<<endl;
+    test.clear();
+    cout<<"current size:"<<test.length()<<endl;
+}
